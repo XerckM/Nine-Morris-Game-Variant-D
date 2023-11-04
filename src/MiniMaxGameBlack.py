8929a317b3fab34a67b53e7ae540a11ea503c2d5
Xerckiem <mercadoxerckiem@gmail.com>
2023-11-04 12:31:47 -0400

    Modified the classes to accomodate invert_board and the functions that were deleted

--- src/MiniMaxGameBlack.py
+++ src/MiniMaxGameBlack.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 from CommonFunctions import (
-    static_estimation_opening_black, generate_moves_midgame_endgame_black,
-    static_estimation_midgame_endgame_black, reset_positions_evaluated,
-    get_positions_evaluated
+    static_estimation_opening, generate_moves_midgame_endgame,
+    static_estimation_midgame_endgame, reset_positions_evaluated,
+    get_positions_evaluated, invert_board
 )
 
 
@@ -11,33 +11,34 @@ class MiniMaxGameBlack:
     def __init__(self):
         pass
 
-    def minimax_game(self, board, depth, is_maximizing):
+    def minimax_game_black(self, board, depth, is_maximizing):
         """Minimax algorithm for the game phase."""
+        board = invert_board(board)
         if depth == 0:
-            if board.count('B') > 3:
-                return static_estimation_midgame_endgame_black(board), board
-            return static_estimation_opening_black(board), board
+            if board.count('W') > 3:
+                return -static_estimation_midgame_endgame(board), board
+            return -static_estimation_opening(board), board
 
-        possible_moves = generate_moves_midgame_endgame_black(board)
+        possible_moves = generate_moves_midgame_endgame(board)
 
         if is_maximizing:
             max_eval = float('-inf')
             best_move = None
             for move in possible_moves:
-                eval_value, _ = self.minimax_game(move, depth - 1, False)
+                eval_value, _ = self.minimax_game_black(move, depth - 1, False)
                 if eval_value > max_eval:
                     max_eval = eval_value
                     best_move = move
-            return max_eval, best_move
+            return max_eval, invert_board(best_move)
 
         min_eval = float('inf')
         best_move = None
         for move in possible_moves:
-            eval_value, _ = self.minimax_game(move, depth - 1, True)
+            eval_value, _ = self.minimax_game_black(move, depth - 1, True)
             if eval_value < min_eval:
                 min_eval = eval_value
                 best_move = move
-        return min_eval, best_move
+        return min_eval, invert_board(best_move)
 
     def main(self, input_file, output_file, depth):
         """Main function to read input, compute best move, and write output."""
@@ -46,7 +47,7 @@ class MiniMaxGameBlack:
         with open(input_file, 'r') as f:
             board = list(f.readline().strip())
 
-        minimax_estimate, best_move = self.minimax_game(board, depth, True)
+        minimax_estimate, best_move = self.minimax_game_black(board, depth, True)
 
         with open(output_file, 'w') as f:
             f.write(''.join(best_move))
@@ -54,4 +55,4 @@ class MiniMaxGameBlack:
         print(f"Input position: {''.join(board)}")
         print(f"Output position: {''.join(best_move)}")
         print(f"Positions evaluated by static estimation: {get_positions_evaluated()}.")
-        print(f"MINIMAX estimate: {minimax_estimate}.")
+        print(f"MINIMAX estimate for black: {minimax_estimate}.")
--- src/MiniMaxOpeningBlack.py
+++ src/MiniMaxOpeningBlack.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 from CommonFunctions import (
-    generate_add_black, static_estimation_opening_black,
-    reset_positions_evaluated, get_positions_evaluated
+    generate_add, static_estimation_opening,
+    reset_positions_evaluated, get_positions_evaluated,
+    invert_board
 )
 
 
@@ -10,31 +11,32 @@ class MiniMaxOpeningBlack:
     def __init__(self):
         pass
 
-    def minimax_opening(self, board, depth, is_maximizing):
+    def minimax_opening_black(self, board, depth, is_maximizing):
         """Minimax algorithm for the opening phase for black."""
+        board = invert_board(board)
         if depth == 0:
-            return static_estimation_opening_black(board), board
+            return -static_estimation_opening(board), board
 
-        possible_moves = generate_add_black(board)
+        possible_moves = generate_add(board)
 
         if is_maximizing:
             max_eval = float('-inf')
             best_move = None
             for move in possible_moves:
-                eval_value, _ = self.minimax_opening(move, depth - 1, False)
+                eval_value, _ = self.minimax_opening_black(move, depth - 1, False)
                 if eval_value > max_eval:
                     max_eval = eval_value
                     best_move = move
-            return max_eval, best_move
+            return max_eval, invert_board(best_move)
 
         min_eval = float('inf')
         best_move = None
         for move in possible_moves:
-            eval_value, _ = self.minimax_opening(move, depth - 1, True)
+            eval_value, _ = self.minimax_opening_black(move, depth - 1, True)
             if eval_value < min_eval:
                 min_eval = eval_value
                 best_move = move
-        return min_eval, best_move
+        return min_eval, invert_board(best_move)
 
     def main(self, input_file, output_file, depth):
         """Main function to read input, compute best move, and write output."""
@@ -44,7 +46,7 @@ class MiniMaxOpeningBlack:
             with open(input_file, 'r') as f:
                 board = list(f.readline().strip())
 
-            minimax_estimate, best_move = self.minimax_opening(board, depth, True)
+            minimax_estimate, best_move = self.minimax_opening_black(board, depth, True)
 
             with open(output_file, 'w') as f:
                 f.write(''.join(best_move))
@@ -52,7 +54,7 @@ class MiniMaxOpeningBlack:
             print(f"Input position: {''.join(board)}")
             print(f"Output position: {''.join(best_move)}")
             print(f"Positions evaluated by static estimation: {get_positions_evaluated()}.")
-            print(f"MINIMAX estimate: {minimax_estimate}.")
+            print(f"MINIMAX estimate for black: {minimax_estimate}.")
 
         except FileNotFoundError:
             print(f"Error: File '{input_file}' not found.")
